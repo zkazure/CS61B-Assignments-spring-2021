b492df66372ed7842c0f8c869e2de7df3c364b81
Kazure Zheng <kazure@debian>
2025-10-08 23:44:10 +0800

    pass Debugexercise1

---
+++ lab2/DebugExercise/DebugExercise1Test.java
@@ -0,0 +1,12 @@
+package DebugExercise;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DebugExercise1Test {
+    @Test
+    public void testDebugExercise1Equals() {
+        assertEquals(3, DebugExercise1.divideThenRound(5, 2));
+    }
+}
+

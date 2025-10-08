f7fa9a489cdb11592ff35ab6dfd1328650f39b72
Kazure Zheng <kazure@debian>
2025-10-08 23:13:10 +0800

    pass the AddConstant

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,11 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+
+    @Test
+    public void testAddConstantWithNullList() {
+        IntList lst = null;
+        IntListExercises.addConstant(lst, 5);
+        assertNull(lst);
+    }
 }

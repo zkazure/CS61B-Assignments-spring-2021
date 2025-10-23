136aa5575878088fe33bcbdf0a7c671d56f21794
Kazure Zheng <kazure@debian>
2025-10-23 18:49:17 +0800

    finish the proj1A ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,70 +2,108 @@ package deque;
 
 public class ArrayDeque<T> {
     private T[] items;
-    private int capacity;
-    private int head;
-    private int tail;
+    private int size;
+    private int nextFirst=0;
+    private int nextLast=1;
 
     private void resize(int new_capacity) {
         T[] new_items = (T[]) new Object[new_capacity];
-        System.arraycopy(items, head, new_items, 0, capacity);
+        int first = (nextFirst+1)%items.length; 
+        if (first+size<=capacity()) {
+            System.arraycopy(items, first, new_items, 0, size);
+        } else {
+            int right = capacity()-first;
+            System.arraycopy(items, first, new_items, 0, right);
+            System.arraycopy(items, 0, new_items, right, size-right);
+        }
+        nextFirst = new_capacity-1;
+        nextLast = size;
         items = new_items;
+    }
+
+    public int capacity() {
+        return items.length;
     }
     
     public ArrayDeque() {
-        capacity = 8;
-        items = (T[]) new Object[capacity];
-        head = 0;
-        tail = 0;
+        items = (T[]) new Object[8];
+        nextFirst = 0;
+        nextLast = 1;
+        size = 0;
     }
 
     public ArrayDeque(ArrayDeque other) {
-        int size = other.size();
-        items = (T[]) new Object[size];
-        System.arraycopy(other.items, other.head, items, 0, size);
+        size = other.size();
+        int capacity = other.capacity();
+        items = (T[]) new Object[capacity];
+        int first = (other.nextFirst+1)%capacity; 
+        if (first+size <= capacity) {
+            System.arraycopy(other.items, first, items, 0, size);
+        } else {
+            int right = capacity-first; 
+            System.arraycopy(other.items, first, items, 0, right);
+            System.arraycopy(other.items, 0, items, right, size-right);
+        }
+        
+        nextFirst = capacity -1;
+        nextLast = size;
     }
 
     public void addFirst(T item) {
-        if ( (tail+1)%capacity == head )
-            resize(capacity * 2);
-        
-        head = (head + capacity - 1) % capacity;
-        items[head] = item;
+        if (size == items.length) {
+            resize(items.length * 2);
+        }
+
+        items[nextFirst] = item;
+        size += 1;
+        nextFirst = (nextFirst-1+items.length)%items.length;
     }
 
     public void addLast(T item) {
-        if ( (tail+1)%capacity == head )
-            resize(capacity * 2);
-
-        tail = (tail+1)%capacity;
-        items[tail] = item;
+        if (size == items.length) {
+            resize(items.length * 2);
+        }
+        items[nextLast] = item; 
+        size += 1; 
+        nextLast = (nextLast+1)%items.length;
     }
 
     public T removeFirst() {
-        T ret = get(head);
-        head = (head+1) % capacity;
+        nextFirst = (nextFirst+1)%items.length;
+        size -= 1;
+        T ret = items[nextFirst];
+        if (items.length>=16 && size*4 < items.length) {
+            resize(items.length / 2);
+        }
+
         return ret;
     }
 
     public T removeLast() {
-        T ret = get(tail);
-        tail = (tail-1+capacity) % capacity;
+        nextLast = (nextLast-1+items.length)%items.length;
+        size -= 1; 
+        T ret = items[nextLast];
+        if (items.length>=16 && size*4 < items.length) {
+            resize(items.length / 2);
+        }
+
         return ret;
     }
 
     public T get(int idx) {
-        if (idx < 0 || idx >= size())
+        if (idx>=size || idx<0)
             return null;
+        if (idx <= nextFirst && idx >= nextLast)
+            return null;
+        idx = (nextFirst +1 + idx) % items.length;
         return items[idx];
     }
 
     public boolean isEmpty() {
-        if (size()>0)
-            return false;
-        return true;
+        return size() == 0;
     }
 
     public int size() {
-        return head+tail+1;
+        return size;
     }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,12 +1,47 @@
 package deque;
 
-import org.juint.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
     @Test
     public void addIsEmptySizeTest() {
         ArrayDeque<String> lld1 = new ArrayDeque<>();
-        assertTrue("A newly initialize should be empty", lld1.isEmpty());
+        assertTrue(lld1.isEmpty());
+    }
+    @Test 
+    public void addRemoveTest() {
+        ArrayDeque<Integer> arr = new ArrayDeque<>();
+        arr.addFirst(0);
+        assertEquals(0, (int)arr.get(0));
+        arr.addLast(1);
+        assertEquals(1, (int)arr.get(1)); 
+
+        arr.removeFirst(); 
+        assertEquals(1, (int)arr.get(0)); 
+        arr.removeLast();
+        assertTrue(arr.isEmpty());
+    }
+    @Test 
+    public void resizeTest() {
+        ArrayDeque<Integer> arr = new ArrayDeque<>(); 
+        for (int i=0; i<33; ++i) {
+            arr.addLast(i);
+        }
+        assertEquals("wrong the capacity is "+arr.capacity(), 64, arr.capacity());
+        assertEquals(33, arr.size());
+    }
+    @Test 
+    public void usageTest() {
+        ArrayDeque<Integer> arr = new ArrayDeque<>(); 
+        for (int i=0; i<32; ++i) {
+            arr.addLast(i);
+        }
+        for (int i=0; i<25; ++i) {
+            arr.removeFirst();
+        }
+        double usage = (double)arr.size()/arr.capacity();
+        assertTrue("wrong usage ratio is "+usage, usage>0.25);
     }
 }
+

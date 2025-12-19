485f263a0bef4096b9732aadcc28e0a4d327c522
Kazure Zheng <kazure@debian>
2025-12-19 19:00:44 +0800

    add all command to main, but wait to complete

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,7 +9,13 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        // DONE: what if args is empty?
+        if (args.length == 0) {
+            Methods.exitWith("Please enter a command.");
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
+
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
@@ -18,7 +24,33 @@ public class Main {
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            case "commit":
+                break;
+            case "rm":
+                break;
+            case "log":
+                break;
+            case "global-log":
+                break;
+            case "find":
+                break;
+            case "status":
+                break;
+            case "checkout":
+                break;
+            case "branch":
+                break;
+            case "rm-branch":
+                break;
+            case "reset":
+                break;
+            case "merge":
+                break;
+            default: {
+                Methods.exitWith("No command with that name exits.");
+                System.out.println("No command with that name exists.");
+                System.exit(0);
+            }
         }
     }
 }
